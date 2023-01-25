caf873a29e334ed6c0671dfdd31a457fc66e58f7
hladkipavel <pgladky22@gmail.com>
2023-01-25 15:36:36 +0100

    update correct redirect to other pages

--- src/main/java/pl/coderslab/web/LoginServlet.java
+++ src/main/java/pl/coderslab/web/LoginServlet.java
@@ -1,5 +1,8 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,5 +15,19 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AdminDao adminDao = new AdminDao();
+        String mail = req.getParameter("mail");
+        String password = req.getParameter("password");
+        Admin admin = new Admin();
+//        Admin admin = adminDao.read(mail, password);
+        if(admin.getId() != 0){
+            resp.sendRedirect("/home");
+        }else {
+            getServletContext().getRequestDispatcher("/home.jsp").forward(req,resp);
+        }
     }
 }
--- src/main/java/pl/coderslab/web/RegistrationServlet.java
+++ src/main/java/pl/coderslab/web/RegistrationServlet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class RegistrationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       resp.sendRedirect("/registrationForm.jsp");
+       getServletContext().getRequestDispatcher("/registrationForm.jsp").forward(req, resp);
     }
 
     @Override
